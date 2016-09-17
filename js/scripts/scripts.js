b0f81fac3965e5a751ecc9db36a7d4f5171cda42
fesyafom <a.feschenko@protonmail.com>
2016-09-17 14:09:10 +0300

    adaptive layout

--- js/scripts/scripts.js
+++ js/scripts/scripts.js
@@ -1,7 +1,27 @@
 var toggleSearchForm = document.getElementById("toggleSearchForm");
+var toggleMenu = document.getElementById("toggleMenu");
+var sliderPrev = document.getElementById("sliderPrev");
+var sliderNext = document.getElementById("sliderNext");
 
 toggleSearchForm.onclick = function(event) {
     var searchTours = document.getElementById("searchTours");
     searchTours.hidden = !searchTours.hidden;
+    return false;
+};
+
+toggleMenu.onclick = function(event) {
+    var navigation = document.getElementById("navigation");
+    if(navigation.style.display === "none" || navigation.style.display === "") {
+        navigation.style.display = "block";
+        return false;
+    }
+    navigation.style.display = "none";
+    return false;
+};
+
+sliderPrev.onclick = function(event) {
+    var slider = document.getElementById("slider");
+    slider.style.marginLeft -= 373;
+    console.log(slider.style.marginLeft);
     return false;
 };
